d51aa7b91ca19e0b8f2b6b94ddaf5adb119aef09
Houjun Liu <houjun@jemoka.com>
2023-03-18 23:49:33 -0700

    some changes

--- evolve.py
+++ evolve.py
@@ -2,28 +2,19 @@ from abc import ABC, abstractmethod, abstractproperty
 from dataclasses import dataclass
 from typing import List
 from enum import Enum
-
-@dataclass
-class Position:
-    x: float
-    y: float
-    z: float
+import numpy as np
 
 @dataclass
 class Context:
-    resources: float # float value for amount of resources available
-    resource_desirability: float # num "not hungry" 0 ---> "hungry" 1
     num_immediate_agents: int # how close the nearby agents are
-    position: Position # where the agent is
-    immediate_position: List[Position] # where the nearby agents are
-    num_cycles: int # number of 
+    position: np.ndarray # where the agent is
+    positions: List[np.ndarray] # where the nearby agents are
+    num_cycles: int # number of iterations psased
 
 class StrategyType(Enum):
     POSITION = 0
-    CONSUMPTION = 1
-    REPRODUCTION = 2
-    COSMETIC = 3
-    OTHER = 4
+    REPRODUCTION = 1
+    OTHER = 2
 
 class AgentStrategy(ABC):
     type = StrategyType.OTHER 
@@ -36,20 +27,56 @@ class AgentStrategy(ABC):
     def state(self):
         pass
 
-class PositionStrategy(AgentStrategy):
+class PositionNearby(AgentStrategy):
     type = StrategyType.POSITION
     
-    def __init__(self, position:Position):
+    def __init__(self, position:np.ndarray, lr=0.1):
+        """A strategy to move towards/away from population centers, weighted by distance
+
+        Arguments:
+        position (np.ndarray): seed position
+        lr (float): moving towards or away from population centers, value multiplied to agent direction 
+        """
+        assert len(position) == 3, "we are in 3 space!"
+
         self.__position = position
+        self.__lr = lr
 
     @property
     def state(self):
         return self.__position
 
-# class Agent:
-#     """
-#     An agent that moves in the environment
-#     """
+    def evolve(self, context):
 
-#     def __init__(self):
+        # calculate regional inluences of each
+        for i in context.positions:
+            # subtracting i means that it will be a vector
+            # pointing TOWARDS i
+            # now we divide by absolute value of i to ensure that
+            # closer points are more valued
+            self.__position += (self.__position - i)*(lr/(np.linalg.norm(i-self.__position)))
+
+class Agent:
+
+    def __init__(self, initial_position:np.ndarray,
+                       initial_metadata=None):
+        self.__position_strategy = None
+        self.__reproduction_strategy = None
+        self.__other_strategy = None
+
+        self.position = initial_position
+        self.other = initial_metadata
+
+    def register(strategy:AgentStrategy, type:StrategyType):
+
+        if type == StrategyType.POSITION:
+            self.__position_strategy = strategy
+        elif type == StrategyType.REPRODUCTION:
+            self.__reproduction_strategy = strategy
+        elif type == StrategyType.OTHER:
+            self.__other_strategy = strategy
+
+    # def __call__(self):
+        
+
 
